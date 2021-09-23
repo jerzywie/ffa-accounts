cc5f77a3cb12cc0fcd549ab3af596f35fc88e537
Jerzy Wieczorek <jerzywie@razzydo.co.uk>
2021-09-23 11:02:43 +0100

    Reintroduce :end-run-tests hook to prevent default exception raise on fail.

--- test/jerzywie/test_runner.cljs
+++ test/jerzywie/test_runner.cljs
@@ -10,7 +10,15 @@
    [jerzywie.ffa-accounts.allocate-async-test]
    [jerzywie.ffa-accounts.analyse-test]
 
+   [cljs.test :as test]
+   [figwheel.main.async-result :as async-result]
    [figwheel.main.testing :refer [run-tests-async]]))
 
 (defn -main [& args]
   (run-tests-async 10000))
+
+
+(defmethod test/report [:cljs.test/default :end-run-tests] [m]
+  (if (test/successful? m)
+    (async-result/send "Tests Passed!!")
+    (async-result/send "Tests FAILED")))
