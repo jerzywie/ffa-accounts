193845945df260cb29f44853a6ff1474398bd5e4
Jerzy Wieczorek <jerzywie@razzydo.co.uk>
2021-09-24 17:45:24 +0100

    Use date of last transaction as default analysis-date.

--- src/jerzywie/ffa_accounts/report_view.cljs
+++ src/jerzywie/ffa_accounts/report_view.cljs
@@ -45,36 +45,40 @@
      (into [:tbody]
            (for [[account-name count amounts date] report-list]
              [:tr
-              [:td  account-name];(r-util/format-account-name)
+              [:td (r-util/format-account-name account-name)]
               [:td.text-right count]
               [:td (r-util/format-donor-amounts amounts)]
               [:td.text-right (util/tonumber (r-util/get-total amounts) "£")]
               [:td (str date)]]))]))
 
-(defn report [data analysis-date]
-  (when (and data analysis-date)
+(defn report [data analysis-date-or-nil]
+  (when data
     (let [allocd-txns (->> data
                            :txns
                            alloc/process-income)
-          processed-transactions (anal/analyse-donations analysis-date allocd-txns)]
-      (state/add-processed-transactions processed-transactions)
-      [:div
-       [:h4 (str "Donations as of " analysis-date)]
-       [:h4 "Account summary"]
-       [:div.row
-        (map (fn [[k v] id] ^{:key id} [:div.col-md-4 (str (capitalize (name k)) ": " (util/tonumber v "£"))])
-             (:accinfo data)
-             (range))]
-       [:div.row
-        [:div.col-md-4]
-        [:div.col-md-4 (str "First transaction: " (-> processed-transactions first :date str))]
-        [:div.col-md-4 (str "Last transaction: " (-> processed-transactions last :date str))]]
-       [:h4 "Current donations"]
-       (filter-donations processed-transactions
-                         (fn [x] (contains? x :current)))
-       [:h4 "One offs"]
-       (filter-donations processed-transactions
-                         (fn [x] (contains? (:freq x) :one-off)))
-       [:h4 "Donor report"]
-       (donor-report)
-       ])))
+          date-first-txn (-> data :txns first :date)
+          date-last-txn (-> data :txns last :date)
+          analysis-date (or analysis-date-or-nil date-last-txn)]
+      (let [processed-transactions (anal/analyse-donations analysis-date allocd-txns)]
+           (state/add-processed-transactions processed-transactions)
+           (state/add-analysis-date! analysis-date)
+           [:div
+            [:h4 (str "Donations as of " analysis-date)]
+            [:h4 "Account summary"]
+            [:div.row
+             (map (fn [[k v] id] ^{:key id} [:div.col-md-4 (str (capitalize (name k)) ": " (util/tonumber v "£"))])
+                  (:accinfo data)
+                  (range))]
+            [:div.row
+             [:div.col-md-4]
+             [:div.col-md-4 (str "First transaction: " date-first-txn)]
+             [:div.col-md-4 (str "Last transaction: " date-last-txn)]]
+            [:h4 "Current donations"]
+            (filter-donations processed-transactions
+                              (fn [x] (contains? x :current)))
+            [:h4 "One offs"]
+            (filter-donations processed-transactions
+                              (fn [x] (contains? (:freq x) :one-off)))
+            [:h4 "Donor report"]
+            (donor-report)
+            ]))))
