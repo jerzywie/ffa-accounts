e24ad69334ac68c6a63355488749b2a559762124
Jerzy Wieczorek <jerzywie@razzydo.co.uk>
2021-09-24 19:23:30 +0100

    Fix copy in deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ rm -rf target/public
 
 clojure -M:fig:min
 
-cp -r resources/public/css $1/css/
-cp -r resources/public/images $1/images/
+cp -r resources/public/css $1/
+cp -r resources/public/images $1/
 cp resources/public/index.html $1
 cp target/public/cljs-out/dev-main.js $1/cljs-out/
